fa181a3eecec72eb61b25c142dfa5183972d78d7
Voyane Rogerio <voyanerogerio@hotmail.com>
2025-01-09 13:54:53 +0200

    Modificado o ficheiro app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # Importação
 from flask import Flask
 
-app = Flask(__name__)
+#Criar a instancia do aplicativo flask
+app = Flask(__name__) 
 
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
-@app.route('/')
-def hello_world():
+@app.route('/') 
+def hello_world(): #definir a funcao
     return 'Hello World'
 
 if __name__ == "__main__":
