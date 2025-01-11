de42f961b4af3262acdc8ffce2acb849dc06e7db
Voyane Rogerio <voyanerogerio@hotmail.com>
2025-01-11 10:05:18 +0200

    Modificado o ficheiro app.py

--- app1.py
+++ app1.py
@@ -15,16 +15,9 @@ CORS(app1)
 
 #USER (id, username, password)
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False, unique = True)
-    password = db.Column(db.String(20), nullable = True)
-
-
-
-
-
-
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
 
 
 #Modelagem
